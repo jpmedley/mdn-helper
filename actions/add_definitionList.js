497bb030117c9e53f6f93ad4f8e1b576d67f944b
Joseph Medley <jmedley@google.com>
2018-09-20 11:09:08 -0700

    Add infrastructure for actions.

---
+++ actions/add_definitionList.js
@@ -0,0 +1,7 @@
+'use strict';
+
+function _run() {
+  console.log("It worked!");
+}
+
+module.exports.run = _run;
--- actions/index.js
+++ actions/index.js
@@ -1,8 +1,10 @@
 'use strict';
 
 require("fs").readdirSync(__dirname).forEach(file => {
+  console.log(file);
   var name = file.replace('.js', '');
   module.exports[name] = require("./" + file);
+  if (file == 'index.js') { return; }
   if (!module.exports[name].run) {
     throw new Error('Module named ' + file + ' does not contain a run() method.');
   }
