08f2d220b679795e23b589fbcb81ea4d319b2ffd
Joseph Medley <jmedley@google.com>
2018-10-16 12:16:53 +0200

    Add npm commands.

---
+++ update-idl.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+IDL_ZIP='https://chromium.googlesource.com/chromium/src/+archive/48c7a8424cd9222a0f9001c923c259265b2222b9/third_party/blink/renderer.tar.gz'
+
+rm -rf idl/
+
+curl $IDL_ZIP > idl.tar.gz
+
+mkdir idl/
+tar xvzf idl.tar.gz -C idl/
