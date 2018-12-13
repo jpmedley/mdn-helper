a4c40c58cddf3b3eec2662ba7faca5298f1a3c27
Joseph Medley <jmedley@google.com>
2018-12-13 14:41:55 -0800

    Correct location of IDL file downloads.

--- update-idl.sh
+++ update-idl.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-IDL_ZIP='https://chromium.googlesource.com/chromium/src/+archive/48c7a8424cd9222a0f9001c923c259265b2222b9/third_party/blink/renderer.tar.gz'
+
+IDL_ZIP='https://chromium.googlesource.com/chromium/src/+archive/HEAD/third_party/blink/renderer.tar.gz'
 
 rm -rf idl/
 
