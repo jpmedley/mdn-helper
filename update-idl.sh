38a493fadaa1e7695a73beb16ac4dad130335413
Joseph Medley <jmedley@google.com>
2018-12-18 10:46:54 -0800

    Finish work on basic testing infra.

--- update-idl.sh
+++ update-idl.sh
@@ -1,13 +1,12 @@
 #!/bin/sh
-IDL_ZIP='https://chromium.googlesource.com/chromium/src/+archive/48c7a8424cd9222a0f9001c923c259265b2222b9/third_party/blink/renderer.tar.gz'
+IDL_ZIP='https://chromium.googlesource.com/chromium/src/+archive/HEAD/third_party/blink/renderer.tar.gz'
 
-rm -rf idl/
+rm -rf idl/core/
+rm -rf idl/build/
 
 curl $IDL_ZIP > idl.tar.gz
 
-mkdir idl/
-tar xvzf idl.tar.gz -C idl/
+tar -C idl/ -xvf idl.tar.gz build/
+tar -C idl/ -xvf idl.tar.gz core/
 
 rm idl.tar.gz
-
-mkdir idl/_test/
