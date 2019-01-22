954b984cdef93b09ae5371976cf43ee92855a9fc
Joseph Medley <jmedley@google.com>
2019-01-22 17:12:14 -0500

    Add directory check to update-data.

--- update-idl.sh
+++ update-idl.sh
@@ -5,7 +5,10 @@ IDL_ZIP='https://chromium.googlesource.com/chromium/src/+archive/HEAD/third_part
 # Don't delete idl/. It contains a test/ directory which is needed.
 rm -rf idl/core/
 rm -rf idl/modules/
-mkdir idl
+
+if [ -d "idl" ]; then
+  mkdir idl
+fi
 
 curl $IDL_ZIP > idl.tar.gz
 
