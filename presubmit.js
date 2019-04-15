8b9439ed57506f174de265036f326e08286e8d44
Joseph Medley <jmedley@google.com>
2019-04-15 15:01:22 -0700

    Update presubmit for new test flags.

--- presubmit.js
+++ presubmit.js
@@ -2,6 +2,9 @@
 
 const config = require('config');
 
-if (config.get('Application.test')) {
+if (config.get('Application.test.active')) {
+  throw new Error('You forgot to turn off testing.');
+}
+if (config.get('Application.test.useTestFiles')) {
   throw new Error('You forgot to turn off testing.');
 }
