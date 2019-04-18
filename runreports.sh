72c42a21522704e8f57acff3974b1f80ac30369b
Joseph Medley <jmedley@google.com>
2019-04-18 09:59:04 -0700

    Create a reports script.

---
+++ runreports.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+npm run burn bcd -- -c api -b all
+npm run burn bcd -- -c css -b all
+npm run burn bcd -- -c html -b all
+npm run burn bcd -- -c javascript -b all
+npm run burn bcd -- -c mathml -b all
+npm run burn bcd -- -c webextensions -b all
