f053e1736f1ef9b15b64f8c5f8d0638947d08eca
Joseph Medley <jmedley@google.com>
2019-04-19 10:12:38 -0700

    Finish reports script.

--- runreports.sh
+++ runreports.sh
@@ -6,3 +6,12 @@ npm run burn bcd -- -c html -b all
 npm run burn bcd -- -c javascript -b all
 npm run burn bcd -- -c mathml -b all
 npm run burn bcd -- -c webextensions -b all
+
+npm run burn chrome
+
+npm run burn urls -- -c api
+npm run burn urls -- -c css
+npm run burn urls -- -c html
+npm run burn urls -- -c javascript
+npm run burn urls -- -c mathml
+npm run burn urls -- -c webextensions
