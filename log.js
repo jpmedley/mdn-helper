16685e468d8d6eade27f04d6db9b0ebc55338fa6
Joseph Medley <jmedley@google.com>
2020-03-24 17:44:48 -0700

    Make it so there is only one logger globally.

--- log.js
+++ log.js
@@ -17,7 +17,9 @@
 const winston = require('winston');
 
 function _initiateLogger() {
-  global.__logger = winston.createLogger();
+  if (!global.__logger) {
+    global.__logger = winston.createLogger();
+  }
 }
 
 module.exports.initiateLogger = _initiateLogger;
