8b6229cb1500ae0ba24e19176340a2ff90160041
Joseph Medley <jmedley@google.com>
2020-05-04 09:33:30 -0700

    Update runreports.sh.

--- runreports.sh
+++ runreports.sh
@@ -24,10 +24,10 @@ npm run burn bcd -- -c mathml -b all
 npm run burn bcd -- -c webextensions -b all
 
 npm run burn chrome
+npm run burn chrome -- -f -o -w fugu-wl.json
 
 npm run burn urls -- -c api
 npm run burn urls -- -c css
 npm run burn urls -- -c html
 npm run burn urls -- -c javascript
 npm run burn urls -- -c mathml
-npm run burn urls -- -c webextensions
